08e769170951405fd192e3cd32d459191979dde1
Werald Opolento <werald.opolento@gmail.com>
2024-12-04 18:23:50 +0800

    add shadcn ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
-import AddNewUserForm from '@/components/AddNewUser';
+import { Button } from '@/components/ui/button';
 
 export default function Home() {
 	return (
-		<div className="flex flex-col items-center justify-center">
-			<h1 className="text-3xl font-bold">Home</h1>
-			<AddNewUserForm />
+		<div>
+			Hello world
+			<Button>Click me!</Button>
 		</div>
 	);
 }
