9d4fec480ac43bf9f2ee989e635102dd714ba6ab
Werald Opolento <werald.opolento@gmail.com>
2024-12-08 19:29:56 +0800

    added registered pages

--- app/registered/page.tsx
+++ app/registered/page.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 
 export default async function RegisteredPage() {
 	const users = await getAllTheUser();
+	console.log(users);
 	return (
 		<div className=" flex flex-col justify-center items-center h-[60vh] gap-4">
 			<h1 className="text-2xl font-bold">Registered Guest</h1>
@@ -21,7 +22,11 @@ export default async function RegisteredPage() {
 				<ul className="flex flex-col gap-2">
 					{users.map((user, i) => (
 						<li key={i} className="group flex flex-row gap-2 ">
-							<span className="bg-teal-700 text-gray-300 font-bold px-4 py-2 group-hover:bg-teal-600  duration-200">
+							<span
+								className={`${
+									user.youPicked == null ? `bg-teal-700` : 'bg-red-500'
+								} text-gray-300 font-bold px-4 py-2 group-hover:bg-teal-600  duration-200`}
+							>
 								{i + 1}
 							</span>
 							<span className="flex bg-gray-200 text-gray-900 px-2 items-center justify-center  w-full group-hover:bg-gray-50  duration-200">
